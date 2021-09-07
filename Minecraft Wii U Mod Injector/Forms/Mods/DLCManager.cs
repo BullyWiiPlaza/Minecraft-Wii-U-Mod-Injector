26fd2fe434ff7b32f665a739b4b175f1f36c1821
miku-666 <74728189+NessieHax@users.noreply.github.com>
2021-09-07 22:12:11 +0200

    Updated function to use less network traffic

--- Minecraft Wii U Mod Injector/Forms/Mods/DLCManager.cs
+++ Minecraft Wii U Mod Injector/Forms/Mods/DLCManager.cs
@@ -7,7 +7,10 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Threading;
 using MetroFramework.Forms;
+using Minecraft_Wii_U_Mod_Injector.Wii_U.Gecko_U;
+using Minecraft_Wii_U_Mod_Injector.Helpers;
 
 namespace Minecraft_Wii_U_Mod_Injector.Forms.Mods
 {
@@ -21,10 +24,40 @@ namespace Minecraft_Wii_U_Mod_Injector.Forms.Mods
         }
 
         private uint currentAmount = 0;
-
         private void updatePackCountLabel()
         {
             PackCountLabel.Text = "Current Pack amount : " + currentAmount.ToString();
+        }
+
+        private void getPackInfos(uint dlcPack,out uint ChildPackCount, out uint LicenseMask, out uint DLCMountIndex, out uint DLCDeviceID, out uint PackID,out uint ParentPack)
+        {
+            byte[] data = MainForm.GeckoU.ReadBytes(dlcPack + 0xf4, 0xa0); // to make less trafic and get everthing we want in one way
+            try
+            {
+                Array.Reverse(data);
+            }
+            catch (Exception error)
+            {
+                MessageBox.Show(error.Message);
+            }
+            ChildPackCount = (BitConverter.ToUInt32(data, 0x94) - BitConverter.ToUInt32(data, 0x98)) >> 2;
+            LicenseMask = BitConverter.ToUInt32(data, 0x14);
+            DLCMountIndex = BitConverter.ToUInt32(data, 0x10);
+            DLCDeviceID = BitConverter.ToUInt32(data, 0xc);
+            PackID = BitConverter.ToUInt32(data, 0);
+            ParentPack = BitConverter.ToUInt32(data, 0x94);
+            }
+
+        private string getPackName(uint dlcPack)
+        {
+            byte[] data = MainForm.GeckoU.ReadBytes(dlcPack + 0x13c, 0x20);
+            uint strLength = ByteSwap.Swap(BitConverter.ToUInt32(data, 0x18));
+            if (strLength > 7)
+            {
+                uint wchar_ptr = ByteSwap.Swap(BitConverter.ToUInt32(data, 0x14));
+                return MainForm.GeckoU.PeekString16((int)strLength, wchar_ptr);
+            }
+            return Encoding.BigEndianUnicode.GetString(data,4,(int)strLength*2);
         }
 
         private void ListPacksBtn_Click(object sender, EventArgs e)
@@ -37,23 +70,12 @@ namespace Minecraft_Wii_U_Mod_Injector.Forms.Mods
             currentAmount = ((end - start) >> 2)-1;
             uint maxAmount = ((maxStorage - start) >> 2);
             updatePackCountLabel();
-            // this is very messy code :|
             for (uint i = 1;i<currentAmount-1;i++)
             {
                 uint dlcPack = MainForm.GeckoU.PeekUInt(start + i * 4);
-                uint strLength = MainForm.GeckoU.PeekUInt(dlcPack + 0x13c + 0x18);
-                string name;
-                if (strLength > 7)
-                {
-                    uint wchar_ptr = MainForm.GeckoU.PeekUInt(dlcPack + 0x13c + 0x14);
-                    name = MainForm.GeckoU.PeekString16((int)strLength*2,wchar_ptr);
-                }
-                else
-                    name = MainForm.GeckoU.PeekString16((int)strLength*2, dlcPack + 0x13c + 4);
-
+                string name = getPackName(dlcPack);
                 DLCPackListView.Items.Add(name).SubItems.Add(dlcPack.ToString());
             }
-
         }
 
         private void setLicenseMaskBtn_Click(object sender, EventArgs e)
@@ -65,32 +87,41 @@ namespace Minecraft_Wii_U_Mod_Injector.Forms.Mods
         private void viewPackInfo(object sender, ListViewItemSelectionChangedEventArgs e)
         {
             removePackBtn.Enabled = true;
-            StringBuilder packInfo = new StringBuilder("DLC Pack Info\n");
+            StringBuilder packInfo = new StringBuilder("DLC Pack Info");
             uint dlcPack = uint.Parse(e.Item.SubItems[1].Text);
-            uint licenseMask = MainForm.GeckoU.PeekUInt(dlcPack + 0x17c);
+            uint ChildpacksCount = 0, licenseMask = 0, DLCMountIndex = 0, DLCDeviceID = 0, PackID = 0, parentPack = 0;
+            getPackInfos(dlcPack, out ChildpacksCount, out licenseMask, out DLCMountIndex, out DLCDeviceID, out PackID, out parentPack);
             setLicenseMaskBtn.Enabled = licenseMask == 0;
-            packInfo.Append("Name : ");
-            packInfo.Append(e.Item.Text + "\n");
-            packInfo.Append("Pack ID : ");
-            packInfo.Append(MainForm.GeckoU.PeekUInt(dlcPack + 0x190) + "\n");
-            packInfo.Append("DLC Mount Index : ");
-            packInfo.Append(MainForm.GeckoU.PeekUInt(dlcPack + 0x180) + "\n");
-            packInfo.Append("DLC Device ID : ");
-            packInfo.Append(MainForm.GeckoU.PeekUInt(dlcPack + 0x184) + "\n");
-            packInfo.Append("LicenseMask : ");
+            packInfo.Append("\nName : ");
+            packInfo.Append(e.Item.Text);
+            packInfo.Append("\nPack ID : ");
+            packInfo.Append(PackID);
+            packInfo.Append("\nDLC Mount Index : ");
+            packInfo.Append(DLCMountIndex);
+            packInfo.Append("\nDLC Device ID : ");
+            packInfo.Append(DLCDeviceID);
+            packInfo.Append("\nLicenseMask : ");
             packInfo.Append(licenseMask);
+            packInfo.Append("\nChild Pack Count : ");
+            packInfo.Append(ChildpacksCount);
+            packInfo.Append("\nParent Pack : ");
+            packInfo.Append(parentPack.ToString("X8"));
             dlcPackInfoLabel.Text = packInfo.ToString();
         }
 
         private void removePackBtn_Click(object sender, EventArgs e)
         {
             uint dlcPack = uint.Parse(DLCPackListView.SelectedItems[0].SubItems[1].Text);
-            removePackBtn.Enabled = setLicenseMaskBtn.Enabled = false;
-            currentAmount -= 1;
-            updatePackCountLabel();
             DLCPackListView.SelectedItems[0].Remove();
+            // first check if it has child packs that are use in texturepacks :)
+            
+
             uint[] args = { 0x10A2AFD0, dlcPack };
             MainForm.GeckoU.CallFunction(0x02ae7e30, args); // DLCManager::removePack(DLCPack *)
+            removePackBtn.Enabled = false;
+            setLicenseMaskBtn.Enabled = false;
+            currentAmount -= 1;
+            updatePackCountLabel();
         }
     }
 }
